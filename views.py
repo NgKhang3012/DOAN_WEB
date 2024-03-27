054821909f7e3551edcf3693bb28525b76d29e27
khang2204 <khang2242004@gmail.com>
2024-03-27 21:39:41 +0700

    Origin code

--- views.py
+++ views.py
@@ -8,11 +8,25 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            raw_password=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data.get('password')
             user=authenticate(username=username,password=raw_password)
             login(request,user)
             return redirect('home')
         else: 
             form = CustomUserCreationForm()
-            return render(request, 'signup.html', {'form': form})
-            
+        return render(request, 'index.html', {'form': form})
+
+def login(request):
+    if request.method=='POST':
+        form=CustomAuthenticationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data.get('password')
+            user=authenticate(username=username,password=raw_password)
+            login(request,user)
+            return redirect('home')
+        else: 
+            form = CustomUserCreationForm()
+        return render(request, 'index.html', {'form': form})
+                     
